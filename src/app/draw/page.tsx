16350c46063ee298654d81e83b9d72fddc40ea5d
Soumyadeep Sharma <soumyadeepsharma99@gmail.com>
2024-05-09 00:13:19 +0530

    canvas changes + updated tldraw

--- src/app/draw/page.tsx
+++ src/app/draw/page.tsx
@@ -1,11 +1,15 @@
 "use client"
 
-import { Tldraw } from "@tldraw/tldraw";
+import { Tldraw, TLComponents } from "tldraw";
+
+const components: TLComponents = {
+    DebugPanel: null
+}
 
 export default function DrawPage() {
     return (
-        <div className="fixed inset-0">
-            <Tldraw />
+        <div className="absolute top-24 w-full h-[90vh] -z-0">
+            <Tldraw components={components} inferDarkMode/>
         </div>
     );
 }
